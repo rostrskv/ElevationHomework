b89857295612cae72becfe5f2c469e0abd3f85ed
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-07-07 17:42:51 +0300

    Remove extra api params

--- week-6/ajax/exercise-4/main.js
+++ week-6/ajax/exercise-4/main.js
@@ -5,16 +5,12 @@ const gifUrl = "https://api.giphy.com/v1/gifs/search"
 const gifTarget = $("#gif-target")
 const searchInput = $("#search-input")
 const searchButton = $("#search-button")
-const limit = 20
 
 
 const getQueryUrl = query => {
     const url = new URL(gifUrl)
     url.searchParams.append("api_key", giphyApiKey)
     url.searchParams.append("q", query)
-    url.searchParams.append("limit", limit)
-    url.searchParams.append("lang", "en")
-    url.searchParams.append("offset", Math.floor(Math.random() * limit))
     return url
 }
 
