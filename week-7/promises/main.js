53b6509dc65b9db742abfdeb797ae80cfddeed17
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-07-12 19:04:43 +0300

    Change to promises

--- week-7/promises/main.js
+++ week-7/promises/main.js
@@ -1,11 +1,13 @@
-const getRandomWord = function (callback) {
+const getRandomWord = function () {
     let words = ['Bonanza', 'Elusive', 'Hindrance', 'Astute', 'Polaroid', 'Phonic', 'Yonder']
-    setTimeout(() => {
-        callback(words[Math.floor(Math.random() * words.length)])
-    }, 1000);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(words[Math.floor(Math.random() * words.length)])
+        }, 1000);
+    })
 }
 
-const getSynonyms = function (word, callback) {
+const getSynonyms = function (word) {
     let thesauraus = {
         'Absolute': ['Definitive', 'Certain', 'Sure', 'Unequivocal'],
         'Astute': ['Sharp', 'Poignant', 'Clever'],
@@ -21,13 +23,14 @@ const getSynonyms = function (word, callback) {
         'Yap': ['Bark', 'Blab', 'Chatter'],
         'Yonder': ['There', 'Away', 'Far', 'Afar']
     }
-
-    setTimeout(() => {
-        callback(thesauraus[word])
-    }, 1000)
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(thesauraus[word])
+        }, 1000);
+    })
 }
 
-const getSentiment = function (word, callback) {
+const getSentiment = function (word) {
     let wordSentiment = {
         'Definitive': 1,
         'Sharp': 1,
@@ -43,24 +46,33 @@ const getSentiment = function (word, callback) {
         'Bark': -1,
         'There': -1
     }
-    setTimeout(() => {
-        callback(JSON.stringify(wordSentiment[word]))
-    }, 1000)
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(wordSentiment[word])
+        }, 1000);
+    })
 }
 
+// No need for promises here
 const getSentimentDescription = function (sentiment) {
     return sentiment === 1 ? "Positive" : sentiment === -1 ? "Negative" : "Neutral"
 }
 
+let word = null
+let firstSynonym = null
 
-
-getRandomWord(function (word) {
-    getSynonyms(word, function (synonyms) {
-        const firstSynonym = synonyms[0]
-        getSentiment(firstSynonym, function (sentimentJSON) {
-            const sentiment = JSON.parse(sentimentJSON)
-            const sentimentDescription = getSentimentDescription(sentiment)
-            console.log(`The word ${word} has a synonym ${firstSynonym} which has a ${sentimentDescription} sentiment`)
-        })
+getRandomWord()
+    .then(w => {
+        word = w
+        return getSynonyms(w)
     })
-})
+    .then(s => {
+        firstSynonym = s[0]
+        return getSentiment(firstSynonym)
+    })
+    .then(sentimentJSON => {
+        const sentiment = JSON.parse(sentimentJSON)
+        const sentimentDescription = getSentimentDescription(sentiment)
+        console.log(`The word ${word} has a synonym ${firstSynonym} which has a ${sentimentDescription} sentiment`)
+    })
+
