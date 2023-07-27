9a58476c1e9fa67ca98309e9dbd5844d9f6a5571
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-07-27 15:14:58 +0300

    Done Spot check 3

--- week-9/react-props/src/App.js
+++ week-9/react-props/src/App.js
@@ -2,13 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Company from './components/Company';
 
-function generateCompanyTags(companies) {
-  return companies.map(company =>
-    <Company name={company.name}></Company>
-  )
-}
+const upperCase = str => str.toUpperCase()
 
-function App() {
+const App = () => {
   let companies = [
     { name: "Tesla", revenue: 140 },
     { name: "Microsoft", revenue: 300 },
@@ -16,9 +12,16 @@ function App() {
 
   return (
     <div className="App">
-      {generateCompanyTags(companies)}
+      {companies.map(company =>
+        <Company
+          name={company.name}
+          revenue={company.revenue}
+          upperCase={upperCase(company.name)}
+        />
+      )
+      }
     </div>
   );
-}
+};
 
 export default App;
