81bbefadbd0244d28b8cfb07cfef3771d7b24b1b
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-07-27 19:14:46 +0300

    Done spot check 3

--- week-9/react-state/src/App.js
+++ week-9/react-state/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import SpotCheck1 from './components/SpotCheck1';
 import SpotCheck2 from './components/SpotCheck2';
+import SpotCheck3 from './components/SpotCheck3';
 
 function App() {
   return (
-    <div className="App">    
-      <SpotCheck1></SpotCheck1>
-      <SpotCheck2></SpotCheck2>
+    <div>    
+      {/* <SpotCheck1></SpotCheck1>
+      <SpotCheck2></SpotCheck2> */}
+      <SpotCheck3></SpotCheck3>
     </div>
   );
 }
---
+++ week-9/react-state/src/components/SpotCheck3.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+function Calendar({ reservations }) {
+    return (
+        <div id="calendar">
+            <h4>Calendar</h4>
+            <ul>
+                {reservations.map(r =>
+                    <li>{
+                        `${r.name} has a reservation on ${r.day} @ ${r.time}`
+                    }</li>)}
+            </ul>
+        </div>
+    )
+}
+
+function Register({ reservations }) {
+    return (<div id="register">
+        <h4>Register</h4>
+        <ul>
+            {reservations.map(r =>
+                <li>{
+                    `${r.day} @ ${r.time}`
+                }</li>)}
+        </ul>
+    </div>)
+}
+
+const SpotCheck3 = () => {
+    const [reservations, setReservations] = useState([
+        { day: "Monday", time: 2000, name: "Earl" },
+        { day: "Monday", time: 1845, name: "Ella" },
+        { day: "Tuesday", time: 1930, name: "Linda" },
+        { day: "Wednesday", time: 2015, name: "Anni" }
+    ]);
+
+    return (
+        <div>
+            <Calendar reservations={reservations} />
+            <Register reservations={reservations} />
+        </div>
+    )
+}
+
+export default SpotCheck3;
