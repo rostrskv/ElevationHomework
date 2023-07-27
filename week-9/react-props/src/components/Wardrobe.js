d7cd62d6dd50a9301c85caacce789d8406fcc464
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-07-27 15:33:13 +0300

    Done Spot check 5

--- week-9/react-props/src/components/Wardrobe.js
+++ week-9/react-props/src/components/Wardrobe.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Article from './Article';
 
-const Wardrobe = ({ wardrobe }) => (    
-    wardrobe.map(({ type, color, size }) => <Article
-        type={type}
-        color={color}
-        size={size}
-        key={`${type}-${color}-${size}`}
-    />)
+const Wardrobe = ({ wardrobe }) => (
+    wardrobe.filter(({ color }) => color === "blue")
+        .map(({ type, color, size }) => <Article
+            type={type}
+            color={color}
+            size={size}
+            key={`${type}-${color}-${size}`}
+        />)
 )
 export default Wardrobe;
