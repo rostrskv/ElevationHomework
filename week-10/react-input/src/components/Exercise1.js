6d3448cb4295631707c4582490a8afe0a0ccd405
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-08-08 20:40:52 +0300

    Done Exercises

---
+++ week-10/react-input/src/components/Exercise1.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+const Exercise1 = () => {
+    const [person, setPerson] = useState({ name: "", age: "" });
+
+    const handleChange = (event, property) => {
+        setPerson({ ...person, [property]: event.target.value });
+    }
+    const handleButtonClick =()=>
+    {
+        alert(`Come in ${person.name}, you’re ${person.age} - that’s good enough`)
+    }
+    return (
+        <div>
+            <input id="name-input" onChange={(e) => handleChange(e, 'name')} value={person.name} />
+            <input id="age-input" onChange={(e) => handleChange(e, 'age')} value={person.age} />
+            <button onClick={handleButtonClick}>Go to Bar</button>
+        </div>
+    );
+}
+export default Exercise1;
---
+++ week-10/react-input/src/components/Exercise2.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+const Exercise2 = () => {
+    const [name, setName] = useState("");
+    const [fruit, setFruit] = useState("");
+    useEffect(() => console.log(`${name} selected ${fruit}`), [fruit])
+
+    return (
+        <div>
+            <input id="name-input" onChange={(e) => setName(e.target.value)} value={name} />
+            <select id="select-input" onChange={(e) => setFruit(e.target.value)} value={fruit}>
+                <option>Apple</option>
+                <option>Banana</option>
+                <option>Cherry</option>
+                <option>Orange</option>
+            </select>
+        </div>
+    );
+}
+export default Exercise2;
