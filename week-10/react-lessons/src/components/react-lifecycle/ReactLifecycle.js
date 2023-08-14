40abe2d75ef3b57ab0b2d56d4cb6c967989400fc
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-08-14 14:35:20 +0300

    Expand headers

--- week-10/react-lessons/src/components/react-lifecycle/ReactLifecycle.js
+++ week-10/react-lessons/src/components/react-lifecycle/ReactLifecycle.js
@@ -4,11 +4,11 @@ export default function ReactLifecycle() {
     return (
         <div>
             <details>
-                <summary>Ex.1</summary>
+                <summary>Ex. 1: Current time</summary>
                 <CurrentTime />
             </details>
             <details open>
-                <summary>Ex.2</summary>
+                <summary>Ex. 2: Top Posts</summary>
                 <TopPosts />
             </details>
 
