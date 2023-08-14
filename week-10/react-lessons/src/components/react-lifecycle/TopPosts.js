91228a0b033e70edfc76f46deca39c82d2eb47a0
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-08-14 09:43:49 +0300

    Add JSDoc, move consts to constants.js

--- week-10/react-lessons/src/components/react-lifecycle/TopPosts.js
+++ week-10/react-lessons/src/components/react-lifecycle/TopPosts.js
@@ -1,21 +1,18 @@
 import { useState, useEffect } from "react";
+import { POSTS_URL, POSTS_COUNT } from "./constants";
 import "./TopPosts.css"
+
 export default function TopPosts() {
-    const [posts, setPosts] = useState([
-        {
-            "userId": NaN,
-            "id": NaN,
-            "title": "",
-            "body": ""
-        }
-    ])
-    const POSTS_URL = "https://jsonplaceholder.typicode.com/posts"
+    const [posts, setPosts] = useState(
+        /** @type{{userId:number, id:number, title:string, body:string}[]?} */
+        (null)
+    )
 
     const getPosts = async () => {
         const postsResponse = await fetch(POSTS_URL)
         if (postsResponse.ok) {
             setPosts(
-                (await postsResponse.json()).slice(0, 10)
+                (await postsResponse.json()).slice(0, POSTS_COUNT)
             )
         }
     }
@@ -27,7 +24,7 @@ export default function TopPosts() {
     return <div class="top-posts">
         <h2>Top Posts</h2>
         <div class="posts-container">
-            {posts.map(p =>
+            {posts?.map(p =>
                 <article key={p.id}>
                     <h3>{p.title}</h3>
                     <p>{p.body}</p>
---
+++ week-10/react-lessons/src/components/react-lifecycle/constants.js
@@ -0,0 +1,2 @@
+export const POSTS_URL = "https://jsonplaceholder.typicode.com/posts"
+export const POSTS_COUNT = 10
